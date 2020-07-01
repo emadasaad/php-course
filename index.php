bb1b2feb3f1b4bf00b7da0587138597962ac2b6d
emadasaad <65672678+emadasaad@users.noreply.github.com>
2020-07-01 20:37:02 +0200

    Update index.php
    add foreach loop

--- index.php
+++ index.php
@@ -12,6 +12,12 @@
 
     echo "<h1>Welcome to ".$name."!</h1>";
     echo "<h2>you have $".$credit." in your wallet.</h2>";
+    $products['computer']=750;
+    $products['car']=15000;
+    $products['iphone']=1000;
+    $products['toaster']=75;
+    echo "<p>A car costs $".$products['car']."</p>";
+    echo "<p>A coputre cost $".$products['computer']."</p>";
     ?>
   </body>
 </html>
